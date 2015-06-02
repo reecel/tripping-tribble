c0a786e4bd4297d9fa4e96772a3cffdbe6668bdc
reecel <lesley.d.reece@gmail.com>
2015-06-01 20:41:45 -0700

    added views

--- entry/models.py
+++ entry/models.py
@@ -8,9 +8,9 @@ class Post(models.Model):
     create_date = models.DateTimeField(default=timezone.now)
     publish_date = models.DateTimeField(blank=True, null=True)
 
-def publish(self):
-    self.publish_date = timezone.now()
-    self.save()
+    def publish(self):
+        self.publish_date = timezone.now()
+        self.save()
 
-def __str__(self):
-    return self.title
+    def __unicode__(self):
+        return self.title
